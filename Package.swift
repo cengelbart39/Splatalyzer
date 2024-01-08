5d4ba240064af899638b63d7c840c49cec4d4123
Christopher Engelbart <cengelbart39@protonmail.com>
2024-01-08 18:24:33 -0500

    Remove GitHub Actions and swift-tools-version 5.9

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Splatalyzer",
+    platforms: [.iOS(.v17), .watchOS(.v10), .macOS(.v14), .tvOS(.v17), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
