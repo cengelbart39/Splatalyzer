e513ceda7351db1c52cd65c51c56ecedbc30e28a
Christopher Engelbart <cengelbart39@protonmail.com>
2024-02-15 21:29:43 -0500

    Make SplatalyzerUI a seperate library in Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,11 @@ let package = Package(
     products: [
         .library(
             name: "Splatalyzer",
-            targets: ["Splatalyzer", "SplatalyzerUI"]),
+            targets: ["Splatalyzer"]),
+        
+        .library(
+            name: "SplatalyzerUI",
+            targets: ["SplatalyzerUI"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
